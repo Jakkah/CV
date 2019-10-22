e1e822308b7e97d700b17e7d113715378a4fa987
jakah <jakah.cgi@gmail.com>
2019-10-22 15:25:45 +0200

    slide + form fini

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function slide(x){
-    if (x === left){
-        
-    } 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+// Modal javascript
+
+var contact = document.getElementById("myContact");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contact");
+
+// Get the <span> element that closes the modal
+var close = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  contact.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+  contact.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == contact) {
+    contact.style.display = "none";
+  }
 }
